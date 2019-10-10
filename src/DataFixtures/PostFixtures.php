cca9724099af315749e36e82d3f987a04f056293
Arifi Gazmen <nemzag@homail.com>
2019-10-10 13:57:44 +0200

    Premier commit.

---
+++ src/DataFixtures/PostFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use App\Entity\Post;
+
+class PostFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    // Injection de dépendance récupère la méthode getDoctrine.
+    {
+        // $product = new Product();
+
+	    $faker = Factory::create('fr_FR');
+
+	    // On a besoin de toute les categories.
+	    // Utilisation de repository des categories pour invoquer la méthode findAll.
+	    $categories = $manager->getRepository(Category::class)->findAll();
+	    // Comme j'utilise ObjectManager, je ne dois pash invoquer getDoctrine
+
+	    for($i = 1; $i <= 20; $i++) {
+	    	$post = new Post();
+	    	$post->setTitle($faker->sentence(4, true));
+	    	$post->setContent($faker->paragraphs(3, true));
+	    	$post->setCreated($faker->dateTimeThisYear('now'));
+	    	$post->setImage($i.'.png');
+	    	$post->setPublished(1);
+	    	$post->setCategory($categories[$faker->numberBetween(0, count($categories) - 1)]);
+		    // nombre aleatoire à générer. Moins 1 car dans la d
+
+	    	$manager->persist($post);
+	    }
+
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+
+    // Ce nom est obligatorie et proposé car j'utilise une interface.
+    public function getDependencies()
+    {
+	    // TODO: Implement getDependencies() method.
+	    return [
+	    	CategoryFixtures::class
+	    ];
+    }
+}
